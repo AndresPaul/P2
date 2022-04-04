f7b1af66e36078b90a6713c6a9a5b8504b4fc72f
Andres Paul <andres.mont.cesp@gmail.com>
2022-04-04 12:25:09 -0400

    User

---
+++ Logic/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Logic
+{
+    public class User
+    {
+        public int Id { get; internal set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
--- Logic/UserManager.cs
+++ Logic/UserManager.cs
@@ -1,8 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Logic
 {
-    public class Class1
+    public class UserManager
     {
+        public List<User> Users{get;set;}
+        public List<User> GetUsers()
+        {
+
+            return new List<User>
+            {
+                 new User { Id = 1, Name = "Juan", Surname = "Doe", Email = "" }
+            };
+        }
+        public User PostUsers(User user)
+        {
+            Users.Add(user);
+            return user;
+        }
+
+        /*public User PutUsers(User user)
+        {
+            return new User { Id = 1, Name = "Juan", Surname = "Doe", Email = "" }
+        }*/
+
     }
 }
